a3adea64490de19074ab53594b10e20edbe1f829
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-03-18 20:36:37 +0700

    menambahkan class DataGeneric

--- TPModul4_1302204062/Program.cs
+++ TPModul4_1302204062/Program.cs
@@ -6,7 +6,22 @@ namespace TPModul4_1302204062
     {
         public static void Main(string[] args)
         {
-            //...
+            DataGeneric<int> data1 = new DataGeneric<int>(1302204062);
+            data1.PrintData();
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        private T Data;
+
+        public DataGeneric(T Data)
+        {
+            this.Data = Data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + this.Data);
         }
     }
 }
