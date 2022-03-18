685d56b67736c37c1ba6ad43df644853079ee098
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-03-18 20:17:54 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TPModul4_1302204062/Program.cs
+++ TPModul4_1302204062/Program.cs
@@ -2,11 +2,19 @@
 
 namespace TPModul4_1302204062
 {
-    public class Program
+    class Program
     {
         public static void Main(string[] args)
         {
-            //...
+            HaloGeneric.SapaUser<string>("Faza");
+        }
+    }
+
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T X)
+        {
+            Console.WriteLine("Halo user " + X);
         }
     }
 }
